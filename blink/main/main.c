a0834b1e2038b4ab22a46183003fb8345470c2fc
yosis <78387492+yosis2003@users.noreply.github.com>
2026-05-09 00:41:54 -0700

    nice start

---
+++ blink/main/main.c
@@ -0,0 +1,146 @@
+/* Scan Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+/*
+    This example shows how to scan for available set of APs.
+*/
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "regex.h"
+
+
+#define WIFI_SSID "Intelligent"
+#define WIFI_PASS "theintelligentquizzermeowruff"
+
+#define DEFAULT_SCAN_LIST_SIZE 10
+
+#ifdef CONFIG_EXAMPLE_USE_SCAN_CHANNEL_BITMAP
+#define USE_CHANNEL_BITMAP 1
+#define CHANNEL_LIST_SIZE 3
+static uint8_t channel_list[CHANNEL_LIST_SIZE] = {1, 6, 11};
+#endif /*CONFIG_EXAMPLE_USE_SCAN_CHANNEL_BITMAP*/
+
+static const char *TAG = "scan";
+static EventGroupHandle_t wifi_event_group;
+#define WIFI_CONNECTED_BIT BIT0
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        esp_wifi_connect(); // retry automatically
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+
+void resolve_host(const char *hostname)
+{
+
+    int status = 1;
+    struct addrinfo hints, *p, *servinfo;
+    char ipstr[INET6_ADDRSTRLEN];
+    memset(&hints, 0, sizeof hints); // make sure the struct is empty
+    hints.ai_family = AF_UNSPEC; // don't care IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    hints.ai_flags = AI_PASSIVE; 
+
+
+
+
+
+    if (status == 0  && servinfo->ai_addr != NULL){
+        for (p = servinfo; p != NULL; p = p->ai_next){
+        void *addr;
+        char *ipver;
+
+        struct sockaddr_in *ipv4;
+        struct sockaddr_in6 *ipv6;
+
+        if (p->ai_family == AF_INET)
+        {
+            ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+
+        }
+        else{
+            ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+        }
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+
+        ESP_LOGI(TAG, "IP ADDDYDYDY %s", ipstr);
+
+
+        }
+
+    }
+}
+
+
+
+void wifi_init_sta(void)
+{
+    wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI("wifi", "wifi_init_sta finished");
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+
+    wifi_init_sta();
+
+    xEventGroupWaitBits(wifi_event_group,
+                        WIFI_CONNECTED_BIT,
+                        pdFALSE,
+                        pdTRUE,
+                        portMAX_DELAY);
+
+    ESP_LOGI("wifi", "CONNECTED TO WIFI!");
+
+    resolve_host("wikipedia.org");
+
+}
